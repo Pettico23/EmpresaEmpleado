90fee5e418796f824f1a4c9c603d69a53e7d09a6
Rodrigo Pettico <rodrigo.pettico@gmail.com>
2025-06-11 17:01:07 -0300

    Empresa Empleados

---
+++ src/absctractempresa/Empleado.java
@@ -0,0 +1,30 @@
+
+package absctractempresa;
+
+import java.time.LocalDate;
+
+
+public abstract class Empleado {
+    private String DNI;
+    private String nombre;
+    private String apellido;
+    private int anioIngreso;
+
+    public Empleado(String DNI, String nombre, String apellido, int anioIngreso) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.anioIngreso = anioIngreso;
+    }
+    public int antiguedadEnAnios(){
+        int anioActual = LocalDate.now().getYear();
+        return anioActual - anioIngreso;
+    }
+    
+    public abstract double getSalario();
+    
+    public String nombreCompleto(){
+        return nombre + " " + apellido;
+    }
+    
+}
---
+++ src/absctractempresa/Empresa.java
@@ -0,0 +1,43 @@
+
+package absctractempresa;
+
+import java.util.ArrayList;
+
+
+public class Empresa {
+    
+    private ArrayList<Empleado> empleados;
+
+    public Empresa() {
+        this.empleados = new ArrayList<>();
+    }
+    public void agregarEmpleado(Empleado e){
+        this.empleados.add(e);
+    }
+    
+    public void mostrarSalarios(){
+        for (Empleado empleado : empleados) {
+            System.out.println(empleado.nombreCompleto() + ":" + empleado.getSalario());
+            
+        }
+        
+    }
+    public Empleado empleadosConMasCientes(){
+        int max = -1;
+        Empleado elMayor = null;
+        for (Empleado e : empleados) {
+            if(e instanceof EmpleadoAComision){
+                EmpleadoAComision eac = (EmpleadoAComision)e;
+                
+                int cant = eac.getCantClientesCaptados();
+                if(cant > max){
+                    max = cant;
+                    elMayor = e;
+                }
+            }
+        }
+        return elMayor;
+    }
+    
+    
+}
